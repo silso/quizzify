fd05e5558cdc7db7edb9eb3ae3a2af8b47d81def
silso <chroelo@gmail.com>
2020-10-30 00:48:59 -0500

    moved css and img out of build, gitignore build

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;;
 
-//init pug
 // NOTE: using gulp so no rendering, just serving static files
 // app.set('views', './views');
 // app.set('view engine', 'pug');
 
 app.use(express.static('build'));
+app.use(express.static('public'));
 
-// app.get('/', function(req, res) {
-//   res.sendFile('./views/index.html');
-// });
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/build/index.html');
+});
 
 app.listen(port, () => console.log(`Quizzify listening on port ${port}!`));
