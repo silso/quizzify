66060742a12e552366f9cee6b5f02d8ceca81f64
silso <chroelo@gmail.com>
2020-11-09 02:56:25 -0600

    rearranged things for layouts (pug extends/block)

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ let gulp = require('gulp'),
     pug = require('gulp-pug');
 
 gulp.task('pug', function() {
-    return gulp.src('views/*.pug')
+    return gulp.src('views/pages/*.pug')
         .pipe(pug())
         .pipe(gulp.dest('build'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('views/*.pug', gulp.series('pug'));
+    gulp.watch('views/**/*.pug', gulp.series('pug'));
 });
