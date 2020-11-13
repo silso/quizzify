4eedcd837eda1dd26a5524a38be2f8dd1c523189
silso <chroelo@gmail.com>
2020-11-13 06:15:07 -0600

    Finished essential functionality of make quiz page

--- public/js/make-quiz.js
+++ public/js/make-quiz.js
@@ -1,17 +1,49 @@
-data = [
-    ['Mazda', 2001, 2000],
-    ['Pegeout', 2010, 5000],
-    ['Honda Fit', 2009, 3000],
-    ['Honda CRV', 2010, 6000],
-];
- 
-let jTable = jexcel(document.getElementById('mySpreadsheet'), {
-    data:data,
-    columns:[
-        { title:'Model'},
-        { title:'Price'},
-        { title:'Model'}
-    ]
+const presets = {
+    'Flashcard': {
+        columns: [{colName: 'Word'}, {colName: 'Definition'}]
+    },
+    'Multiple choice': {
+        columns: [
+            {colName: 'Question'},
+            {colName: 'Correct Answer'},
+            {colName: 'Wrong Answer 1'},
+            {colName: 'Wrong Answer 2'},
+            {colName: 'Wrong Answer 3'},
+        ]
+    },
+    'Geography': {
+        columns: [
+            {colName: 'Name'},
+            {colName: 'Region'},
+        ]
+    },
+    'Image': {
+        columns: [
+            {colName: 'Word'},
+            {colName: 'Image URL'},
+        ]
+    }
+}
+
+let jTable = jexcel(document.getElementById('quizDataSpreadsheet'), {
+    data: Array.from({length: 10}, () => ['']),
+    columns: [{title:'hidden', width: 1}],
+    columnSorting: false,
+    oninsertcolumn: function(a) {
+        const colHeads = document.querySelectorAll('#quizDataSpreadsheet thead > tr > td:not(:first-child)');
+        for (let i = 0; i < colHeads.length; i++) {
+            let colHead = colHeads[i];
+            if (colHead.childElementCount=== 0) {
+                colHead.insertAdjacentHTML('beforeend', `
+                    <img src='/svg/wrench.svg' data-id='${i-1}' role='button' style = '
+                        position: absolute;
+                        right: 0.3em;
+                        top: 0.4em;
+                    ' data-toggle='modal' data-target='#colSettingsModal'>
+                `);
+            }
+        }
+    }
 });
 
 function autoResizeJexcel(jexcelContainer, jexcelTable) {
@@ -30,9 +62,9 @@ function autoResizeJexcel(jexcelContainer, jexcelTable) {
         const colSum = colWidths.reduce((a, b) => a + b);
         const leftColWidth = document.querySelector('.jexcel_selectall').offsetWidth;
         const newColSum = jexcelContainer.clientWidth - leftColWidth;
-        console.log(colSum);
 
-        for (let i = 0; i < colWidths.length; i++) {
+        jexcelTable.setWidth(0, 1);
+        for (let i = 1; i < colWidths.length; i++) {
             jexcelTable.setWidth(i, Math.max(colWidths[i]*newColSum/colSum, 20));
         }
     });
@@ -42,3 +74,21 @@ function autoResizeJexcel(jexcelContainer, jexcelTable) {
 }
 
 autoResizeJexcel(document.getElementById('spreadsheetContainer'), jTable);
+
+function applyPreset(table, settings) {
+    const numCols = document.querySelectorAll('#quizDataSpreadsheet thead > tr > td').length - 2;
+    if (numCols) table.deleteColumn(1, numCols);
+    for (col of settings.columns) {
+        table.insertColumn(1, -1, false, {title: col.colName});
+    }
+}
+
+$('#colSettingsModal').on('show.bs.modal', function (e) {
+    console.log(e.relatedTarget.dataset.id);
+});
+
+const presetSelect = document.getElementById('presetSelect');
+presetSelect.addEventListener('change', function(e) {
+    applyPreset(jTable, presets[this.value]);
+});
+presetSelect.dispatchEvent(new Event('change'));
